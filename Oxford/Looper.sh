08bc39fa54d6ba473a96640bd13195ff6a1371fe
Martin Bies <MBies87@googlemail.com>
2020-04-07 13:56:15 +0200

    Remove tmp files on Oxford cluster

--- Oxford/Looper.sh
+++ Oxford/Looper.sh
@@ -25,51 +25,27 @@ overall=$(($threads * $compsPerThread - 1))
 finished=false
 while ( ! $finished )
 do
+    
     # execute gap scanner for 5 minutes then stop it
-    for i in $(seq 1 $threads); do
-        
-        # form path to scan file
-        file_path="$path/Scan$i/Scan.gi"
-        
-        # start background job
-        #(timeout "${lapse}s" $gap_str $file_path) &
-        ($gap_str $file_path) &
-        
-    done
-    sleep 20
-    #wait
+    timeout "${lapse}s" $gap_str $file_path
     
-    #some_command &
-#P1=$!
-#other_command &
-#P2=$!
-#wait $P1 $P2
-    
-    # but we need to run potentially several gaps in parallel for this limited time and not sequentially after each other!
-    # See https://www.cyberciti.biz/faq/how-to-run-command-or-code-in-parallel-in-bash-shell-under-linux-or-unix/
-    # -> so essentially just place & at the end for a background job and done
+    # remove tmp-files
+    rm -rf $(find /tmp -name 'gap_4ti2*' -execdir pwd \; )
     
     # read out status of all threads - save them to an array and write log-file
     total=0
     $(which date) >> "$path/MyScan.log"
     for i in $(seq 1 $threads); do
-        
-        # set up status file
-        status_file="$path/Scan$i/StatusOfRun$i.txt"
-        
-        # write to log file
         echo "Scan$i:" >> "$path/MyScan.log"
+        status_file="$path/Scan$i/StatusOfRun$i.txt"
         $(which cat) "$path/Scan$i/StatusOfRun$i.txt" >> "$path/MyScan.log";
-        echo "\n" >> "$path/MyScan.log"
-        
-        # increase total-counter
         thread_status=$(cat "$status_file")
         total=$((total+thread_status))
     done
+    echo "\n" >> "$path/MyScan.log"
     
     # check if computation is finished to stop loop
-    #if [ "$total" -gt "$overall" ]
-    if [ "$total" -gt 0 ]
+    if [ "$total" -gt "$overall" ]
     then
         finished=true
         echo "computation finished"
